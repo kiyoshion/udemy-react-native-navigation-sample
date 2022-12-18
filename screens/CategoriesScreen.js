8272587480e325e4688dd4952b03a0553c1ab1c9
kiyoshion <kiyoshion@gmail.com>
2022-12-18 22:12:40 +0900

    Add CategoriesScreen

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,19 @@
+import { FlatList } from "react-native";
+import CategoryGridTile from "../components/CategoryGirdTile";
+
+import { CATEGORIES } from "../data/dummy-data";
+
+export default function CategoriesScreen() {
+
+  const renderCategoryItem = (itemData) => {
+    return <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
+  };
+
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderCategoryItem}
+    />
+  );
+}
