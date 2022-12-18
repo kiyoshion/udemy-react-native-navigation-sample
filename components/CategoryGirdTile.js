0a09c26d8e5823888ad4092cb591715813bfc749
kiyoshion <kiyoshion@gmail.com>
2022-12-18 22:41:14 +0900

    Add navigation packages

--- components/CategoryGirdTile.js
+++ components/CategoryGirdTile.js
@@ -1,13 +1,49 @@
-import { Pressable, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View, Platform } from "react-native";
 
 export default function CategoryGridTile({ title, color }) {
   return (
-    <View>
-      <Pressable>
-        <View>
-          <Text>{title}</Text>
+    <View style={styles.gridItem}>
+      <Pressable
+        style={({ pressed }) => [styles.button, pressed ? styles.buttonPressed: '']}
+        android_ripple={{ color: '"ccc' }}
+      >
+        <View style={[styles.innerContainer, {backgroundColor: color}]}>
+          <Text style={styles.title}>{title}</Text>
         </View>
       </Pressable>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    margin: 16,
+    height: 150,
+    borderRadius: 8,
+    backgroundColor: 'white',
+    elevation: 4,
+    shadowColor: 'black',
+    shadowOpacity: .2,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    overflow: Platform.OS === 'android' ? 'hidden' : 'visible',
+  },
+  button: {
+    flex: 1,
+  },
+  buttonPressed: {
+    opacity: 0.5,
+  },
+  innerContainer: {
+    flex: 1,
+    padding: 16,
+    borderRadius: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+});
