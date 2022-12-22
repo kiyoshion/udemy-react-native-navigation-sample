9f05cec63c24c9ce5bb3de5fa8f60f7872ddccba
kiyoshion <kiyoshion@gmail.com>
2022-12-22 18:12:34 +0900

    Add BottomTabs

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import CategoriesScreen from './screens/CategoriesScreen';
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
@@ -9,34 +10,36 @@ import MealDetailScreen from './screens/MealDetailScreen';
 
 const Stack = createNativeStackNavigator();
 
+const BottomTab = createBottomTabNavigator();
+
 export default function App() {
   return (
     <>
       <StatusBar style='light' />
       <NavigationContainer>
-        <Stack.Navigator
+        <BottomTab.Navigator
           screenOptions={{
             headerStyle: { backgroundColor: '#350414' },
             headerTintColor: 'white',
             contentStyle: { backgroundColor: '#350414' },
           }}
         >
-          <Stack.Screen
+          <BottomTab.Screen
             name='MealsCategories'
             component={CategoriesScreen}
             options={{
               title: 'All Meals',
             }}
-          />
-          <Stack.Screen
-            name='MealsOverview'
-            component={MealsOverviewScreen}
-          />
-          <Stack.Screen
-            name='MealDetail'
-            component={MealDetailScreen}
-          />
-        </Stack.Navigator>
+            />
+            <Stack.Screen
+              name='MealsOverview'
+              component={MealsOverviewScreen}
+            />
+            <Stack.Screen
+              name='MealDetail'
+              component={MealDetailScreen}
+            />
+        </BottomTab.Navigator>
       </NavigationContainer>
     </>
   );
